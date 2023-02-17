2189ef3fa78b90d6380914853336d003f99c2908
abba <abba@nuclearinstruments.eu>
2023-02-17 19:28:05 +0100

    compile deb

--- compile_all_architecures.sh
+++ compile_all_architecures.sh
@@ -14,7 +14,10 @@ mkdir -p build_x64
 cd build_x64
 ../configure --prefix=$(pwd)/output
 make -j16 && make install
-cd ..
+cd output
+tar zcvf ../../libr5560-x64.tar.gz .
+cd ../..
+
 
 #compile x86 
 rm -rf build_i386
@@ -22,7 +25,9 @@ mkdir -p build_i386
 cd build_i386
 ../configure --host=i686-linux-gnu CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32 --prefix=$(pwd)/output
 make -j16 && make install
-cd ..
+cd output
+tar zcvf ../../libr5560-i386.tar.gz .
+cd ../..
 
 
 #compile arm64
@@ -33,7 +38,9 @@ export CC=aarch64-linux-gnu-gcc
 export CXX=aarch64-linux-gnu-g++
 ../configure --host=aarch64-linux-gnu --prefix=$(pwd)/output --build=$BUILD
 make -j16 && make install
-cd ..
+cd output
+tar zcvf ../../libr5560-arm64.tar.gz .
+cd ../..
 
 #compile armhf
 rm -rf build_armhf
@@ -43,4 +50,6 @@ export CC=aarm-linux-gnueabihf-gcc
 export CXX=arm-linux-gnueabihf-g++
 ../configure --host=arm-linux-gnueabihf --prefix=$(pwd)/output --build=$BUILD
 make -j16 && make install
-cd ..
+cd output
+tar zcvf ../../libr5560-armhf.tar.gz .
+cd ../..
